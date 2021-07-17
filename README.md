c723be4182d06d16b6f8a3988f4447679dd0be4f
Your Name <desarrollador2@alcanosesp.com>
2021-07-17 17:05:54 -0500

    react dashboard first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Theme Dashboard React
+React dashboard Theme by @darwinyusef. called [Volt Dashboard](https://github.com/themesberg/volt-bootstrap-5-dashboard).
+
+## Powered by React.js
+
+Volt React is an extension of the popular [react-bootstrap](https://react-bootstrap.github.io/) library and it is based on the newest version of Bootstrap 5. all modified
---
+++ src/data/bugs.js
@@ -0,0 +1,27 @@
+
+export default [
+    {
+        "id": 1,
+        "read": false,
+        "sender": "Construction of elements",
+        "time": "a few moments ago",
+        "link": "#",
+        "message": `Added you to an event "Project stand-up" tomorrow at 12:30 AM.`
+    },
+    {
+        "id": 2,
+        "read": false,
+        "sender": "Construction of elements",
+        "time": "2 hrs ago",
+        "link": "#",
+        "message": `Added you to an event "Project stand-up" tomorrow at 12:30 AM.`
+    },
+    {
+        "id": 3,
+        "read": false,
+        "sender": "Construction of elements",
+        "time": "5 hrs ago",
+        "link": "#",
+        "message": `Added you to an event "Project stand-up" tomorrow at 12:30 AM.`
+    }
+]
